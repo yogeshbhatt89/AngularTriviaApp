fcd139c46cc57fe94c515bfe2cf1579db9045805
Yogesh Bhatt <yogeshbhatt89@gmail.com>
2022-06-14 21:56:46 -0400

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAbk7jxzQV8EuUunka6vxPktr_yW8UEKl8',
+    authDomain: 'triviaapp-9819f.firebaseapp.com',
+    projectId: 'triviaapp-9819f',
+    storageBucket: 'triviaapp-9819f.appspot.com',
+    messagingSenderId: '901953651827',
+    appId: '1:901953651827:web:ed6728892f9af926cc5606',
+    measurementId: 'G-GJPF0ZR3CR',
+  },
 };
